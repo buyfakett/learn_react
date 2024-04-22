1bec4d4440067d1264330596c60133e59794da76
buyfakett <buyfakett@vip.qq.com>
2024-04-22 22:14:36 +0800

    day 2-3

--- src/Day2.3.js
+++ src/Day2.3.js
@@ -1,4 +1,6 @@
 import {useEffect, useState} from "react";
+import axios from "axios";
+import listData from "./data/listData.json"
 
 function Son() {
     // 定时器
@@ -6,7 +8,7 @@ function Son() {
     useEffect(() => {
         const timer = setInterval(() => {
             setCount(count + 1)
-            console.log("定时器正在执行", {count})
+            // console.log("定时器正在执行", {count})
         }, 1000)
         return () => {
             // 清除定时器
@@ -20,15 +22,50 @@ function Son() {
         </div>
     )
 }
+// 封装自定义hooks
+function useTaggle(){
+    const [value, setValue] = useState(true)
+
+    const toggle = () => setValue(!value)
+
+    return {
+        value,
+        toggle
+    }
+}
 
 
 function Day23() {
     const [state, setState] = useState(true)
 
+    const {value, toggle} = useTaggle()
+
+    const [testData, setTestData] = useState([])
+
+     useEffect(() => {
+        async function getList() {
+            // const response = await axios.get('https://xxx')
+            const response = await listData
+            setTestData(listData.data.items)
+        }
+        getList()
+    }, []);
+
     return (
         <div className="App">
             { state && <Son/>}
             <button onClick={() => setState(false)}>卸载组件</button>
+
+            <div>
+                {value && <div>this is div</div> || <div>edit</div>}
+                <button onClick={toggle}>toggle</button>
+            </div>
+
+            <div>
+                <ul>
+                    {testData.map(item => <li key={item.id}>{item.hostname}</li>)}
+                </ul>
+            </div>
         </div>
 
     )
